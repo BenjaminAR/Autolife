0e80a6f6de90bbfd8f512dd9352b000228e6abff
Benjamin Avila <developerbenajminar@gmail.com>
2023-03-22 09:54:16 -0600

    Contraseña

--- enviarIonos.php
+++ enviarIonos.php
@@ -75,8 +75,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $mail->isSMTP();                                      // Send using SMTP
     $mail->Host = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'soporte@amsamex.com.mx';           // SMTP username
-    $mail->Password = '6@pUVTvS7U7a';                     // SMTP password
+    $mail->Username = 'correo@dominio.com';           // SMTP username
+    $mail->Password = 'Contraseña';                     // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
